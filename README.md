9d7e8938e7ac6ea2804e370a6b2c3481c668820f
MarcioLuizBR <marcio.asriel@gmail.com>
2023-12-25 21:38:08 -0300

    mais prints e correcao das querys

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# banco-de-dados-SQL
-resolução do teste da Rox para engenharia de dados
+
+
+# teste Rox Partner
+
+Repositorio para amarzenar os passos desenvolvidos no teste para a vaga de engenheiro de dados pela empresa [Rox Partner](https://roxpartner.com/)
+
+## sequencia do teste
+
+- criação de um banco de dados local gerando assim a base de teste, escolhi usar o [XAMPP](https://www.apachefriends.org/pt_br/index.html) para criar um mysql em meu notebook.
+
+- criação das tabelas , deixei um print na pasta [PRINT passos](https://github.com/MarcioLuizBR/banco-de-dados-SQL/tree/main/PRINT%20passos)
+para a criação das tabelas, segui uma ordem , 1 criava e populava a tabela, assim verificava o que era necessario arrumar na configuração das colunas. os arquivos em [SQL ultilizados](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/criarbanco.sql) estao no repositorio
+
+- popular as tabelas, deixei um print na pasta [PRINT passos](https://github.com/MarcioLuizBR/banco-de-dados-SQL/tree/main/PRINT%20passos) para popular , os arquivos estao [populartabela.sql](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/populartabela.sql)
+
+- terminado a estrutura, tirei um print do [desenho final](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/PRINT%20passos/03%20estrutura%20da%20tabela.jpg)
+
+- criação do [Banco de dados SQL](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/PRINT%20passos/05%20-%20BDtesterox%20azure.jpg) no azure e controle via [SSMS](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/PRINT%20passos/06%20-%20SSMS%20com%20os%20banco%20formado.jpg)
+
+- fiz a exportacao do banco de dados local em arquivo [testerox.sql](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/testerox.sql) que esta no repositorio 
+
+- as querys criadas para responder o teste estao todas no arquivo [querys.sql](https://github.com/MarcioLuizBR/banco-de-dados-SQL/blob/main/querys.sql) e o print dos relatorios gerados estao na [pasta](https://github.com/MarcioLuizBR/banco-de-dados-SQL/tree/main/PRINT%20passos)
+
+
+
+
+bom, desde ja agradeço a oportunidade
--- querys.sql
+++ querys.sql
@@ -7,26 +7,27 @@ HAVING COUNT(*) >= 3;
 
 
 --2. Escreva uma query que ligue as tabelas Sales.SalesOrderDetail, Sales.SpecialOfferProduct e Production.Product e retorne os 3 produtos (Name) mais vendidos (pela soma de OrderQty), agrupados pelo número de dias para manufatura (DaysToManufacture).
-SELECT 
+SELECT
     p.Name AS ProductName,
     p.ProductID,
     p.DaysToManufacture,
     SUM(sod.OrderQty) AS TotalOrderQty
-FROM 
+FROM
     SalesOrderDetail sod
-JOIN 
+JOIN
     SpecialOfferProduct sop ON sod.SpecialOfferID = sop.SpecialOfferID
-JOIN 
+JOIN
     Product p ON sop.ProductID = p.ProductID
-GROUP BY 
+GROUP BY
     p.ProductID, p.Name, p.DaysToManufacture
-ORDER BY 
+ORDER BY
     TotalOrderQty DESC
 LIMIT 3;
 
+
 --3 Escreva uma query ligando as tabelas Person.Person, Sales.Customer e Sales.SalesOrderHeader de forma a obter uma lista de nomes de clientes e uma contagem de pedidos efetuados.
 SELECT
-    p.FirstName + ' ' + p.LastName AS CustomerName,
+    CONCAT(p.FirstName, ' ', p.LastName) AS CustomerName,
     COUNT(soh.SalesOrderID) AS OrderCount
 FROM
     Person p
@@ -37,31 +38,31 @@ JOIN
 GROUP BY
     p.FirstName, p.LastName;
 
+
 --4 Escreva uma query usando as tabelas Sales.SalesOrderHeader, Sales.SalesOrderDetail e Production.Product, de forma a obter a soma total de produtos (OrderQty) por ProductID e OrderDate.
-SELECT
+SELECT 
     sod.ProductID,
-    p.Name AS ProductName,
     soh.OrderDate,
     SUM(sod.OrderQty) AS TotalOrderQty
-FROM
-    Sales.SalesOrderHeader soh
-JOIN
-    Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
-JOIN
-    Production.Product p ON sod.ProductID = p.ProductID
-GROUP BY
-    sod.ProductID, p.Name, soh.OrderDate;
+FROM 
+    SalesOrderHeader soh
+JOIN 
+    SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+JOIN 
+    Product p ON sod.ProductID = p.ProductID
+GROUP BY 
+    sod.ProductID, soh.OrderDate;
 
 --5 Escreva uma query mostrando os campos SalesOrderID, OrderDate e TotalDue da tabela Sales.SalesOrderHeader. Obtenha apenas as linhas onde a ordem tenha sido feita durante o mês de setembro/2011 e o total devido esteja acima de 1.000. Ordene pelo total devido decrescente.
-SELECT
+SELECT 
     SalesOrderID,
     OrderDate,
     TotalDue
-FROM
-    Sales.SalesOrderHeader
-WHERE
-    YEAR(OrderDate) = 2011
-    AND MONTH(OrderDate) = 9
+FROM 
+    SalesOrderHeader
+WHERE 
+    YEAR(OrderDate) = 2011 AND MONTH(OrderDate) = 9
     AND TotalDue > 1000
-ORDER BY
+ORDER BY 
     TotalDue DESC;
+
